bcc8333909d25191db120ec6764805dbc4d57576
bhagat2296 <103653090+bhagat2296@users.noreply.github.com>
2022-08-02 21:57:25 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,188 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*void printN(int);                                                 //Question 1
+int main()
+{
+    int n;
+    printf("enter value of N\n");
+    scanf("%d",&n);
+    printN(n);
+    return 0;
+}
+
+void printN(int n)
+{
+
+    if(n>0)
+        {
+         printN(n-1);
+         printf("\n%d\n",n);
+        }
+}*/
+
+
+/*void printRN(int);                                                 //Question 2
+int main()
+{
+    int n;
+    printf("enter value of N\n");
+    scanf("%d",&n);
+    printRN(n);
+    return 0;
+}
+
+void printRN(int n)
+{
+
+    if(n>0)
+        {
+
+            printf("\n%d\n",n);
+            printRN(n-1);
+        }
+}*/
+
+/*void oddN(int);                                                 //Question 3
+int main()
+{
+    int n;
+    printf("enter value of N\n");
+    scanf("%d",&n);
+    oddN(n);
+    return 0;
+}
+
+void oddN(int n)
+{
+
+    if(n>0)
+    {
+            oddN(n-1);
+            printf("\n%d\n",(2*n-1));
+    }
+
+
+}*/
+
+
+
+/*void oddN(int);                                                 //Question 4
+int main()
+{
+    int n;
+    printf("enter value of N\n");
+    scanf("%d",&n);
+    oddN(n);
+    return 0;
+}
+
+void oddN(int n)
+{
+
+    if(n>0)
+    {
+        printf("\n%d\n",(2*n-1));
+            oddN(n-1);
+
+    }
+
+
+}*/
+
+
+
+/*void evenN(int);                                                 //Question 5
+int main()
+{
+    int n;
+    printf("enter value of N\n");
+    scanf("%d",&n);
+    evenN(n);
+    return 0;
+}
+
+void evenN(int n)
+{
+
+    if(n>0)
+    {
+            evenN(n-1);
+            printf("\n%d\n",(2*n));
+    }
+
+
+}*/
+
+
+
+/*void evenR(int);                                                 //Question 6
+int main()
+{
+    int n;
+    printf("enter value of N\n");
+    scanf("%d",&n);
+    evenR(n);
+    return 0;
+}
+
+void evenR(int n)
+{
+
+    if(n>0)
+    {
+        printf("\n%d\n",(2*n));
+            evenR(n-1);
+
+    }
+
+
+}*/
+
+
+
+/*void squreN(int);                                                 //Question 7
+int main()
+{
+    int n;
+    printf("enter value of N\n");
+    scanf("%d",&n);
+    squareN(n);
+    return 0;
+}
+
+void squareN(int n)
+{
+
+    if(n>0)
+        {
+         squareN(n-1);
+         printf("\n%d\n",(n*n));
+        }
+}*/
+
+
+
+/*int main()                                              //Question 10
+{
+    int n;
+    printf("enter value of N\n");
+    scanf("%d",&n);
+    printR(n);
+    return 0;
+}
+
+void printR(int n)
+{
+
+    if(n>0)
+        {
+          printf("\n%d\n",n);
+          printR(n-1);
+
+}
+
+}*/
+
+
+
+                                                            //Question 8,9 has not solved.
